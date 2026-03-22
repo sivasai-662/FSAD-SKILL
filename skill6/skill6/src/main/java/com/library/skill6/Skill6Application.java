3336cb119f3d2abaf813bf5e913c850b75586fe6
sivasai-662 <bsivasai79@gmail.com>
2026-03-22 18:31:35 +0530

    first commit

---
+++ skill6/skill6/src/main/java/com/library/skill6/Skill6Application.java
@@ -0,0 +1,13 @@
+package com.library.skill6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skill6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skill6Application.class, args);
+	}
+
+}
